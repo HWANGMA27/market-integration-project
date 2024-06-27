80b31ff7675c667e18161965272aa5cf30a9231c
hwangma27 <pikacu22@kakao.com>
2024-06-27 15:18:57 +0900

    [ADD] Api Endpoint 정보 및 호출 메서드 추가

---
+++ spring-scheduler/src/main/java/prj/blockchain/exchange/config/ApiProperties.java
@@ -0,0 +1,14 @@
+package prj.blockchain.exchange.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "api.endpoint")
+@Getter
+@Setter
+public class ApiProperties {
+    private String price;
+}
--- spring-scheduler/src/main/java/prj/blockchain/exchange/task/ScheduledTasks.java
+++ spring-scheduler/src/main/java/prj/blockchain/exchange/task/ScheduledTasks.java
@@ -1,13 +1,29 @@
 package prj.blockchain.exchange.task;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import prj.blockchain.exchange.config.ApiProperties;
 
 @Component
+@RequiredArgsConstructor
 public class ScheduledTasks {
+
+    private final WebClient webClient;
+    private final ApiProperties endPoint;
+    private final String targetCurrency = "btc";
+    private final String paymentCurrency = "krw";
 
     @Scheduled(cron = "${scheduler.cron}")
     public void performScheduledTask() {
-        System.out.println("Scheduled task is running...");
+        String getPriceEndPoint = String.join("/", endPoint.getPrice(), targetCurrency, paymentCurrency);
+        webClient.get()
+                .uri(getPriceEndPoint)
+                .retrieve()
+                .bodyToMono(String.class)
+                .doOnNext(response -> System.out.println("Response: " + response))
+                .doOnError(error -> System.err.println("Error: " + error.getMessage()))
+                .subscribe();
     }
 }
