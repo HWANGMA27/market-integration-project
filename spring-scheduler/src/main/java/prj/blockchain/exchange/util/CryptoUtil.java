a5dc46cf7b15a62eaa308cbae5f1599a3897134f
hwangma27 <pikacu22@kakao.com>
2024-07-01 22:23:18 +0900

    [ADD] 계정 키 암호화에 필요한 시크릿 관련 util&config 추가

---
+++ spring-scheduler/src/main/java/prj/blockchain/exchange/util/CryptoUtil.java
@@ -0,0 +1,47 @@
+package prj.blockchain.exchange.util;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class CryptoUtil {
+
+    private static final String ALGORITHM = "AES";
+    private static final int KEY_SIZE = 128;
+
+    // AES 비밀 키 생성
+    public static SecretKey generateKey() throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
+        keyGen.init(KEY_SIZE);
+        return keyGen.generateKey();
+    }
+
+    // 문자열 키를 SecretKey로 변환
+    public static SecretKey getKeyFromString(String key) {
+        byte[] decodedKey = Base64.getDecoder().decode(key);
+        return new SecretKeySpec(decodedKey, 0, decodedKey.length, ALGORITHM);
+    }
+
+    // SecretKey를 문자열로 변환
+    public static String getStringFromKey(SecretKey key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    // 암호화
+    public static String encrypt(String data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    // 복호화
+    public static String decrypt(String encryptedData, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decodedData = Base64.getDecoder().decode(encryptedData);
+        byte[] decryptedData = cipher.doFinal(decodedData);
+        return new String(decryptedData);
+    }
+}
