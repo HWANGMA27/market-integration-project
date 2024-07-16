169590db04e002eae7db655bde61b16601ee29f7
hwangma27 <pikacu22@kakao.com>
2024-07-16 23:37:25 +0900

    [ADD] Init UPBIT-API Project

---
+++ upbit-api/src/main/java/prj/blockchain/upbit/UpbitApiApplication.java
@@ -0,0 +1,13 @@
+package prj.blockchain.upbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbitApiApplication.class, args);
+	}
+
+}
