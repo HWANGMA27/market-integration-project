1f619b6ec6bdc1f52d91471a683e25500fb6b1d0
hwangma27 <pikacu22@kakao.com>
2024-07-18 23:54:36 +0900

    [ADD] Init Slack-integration Project

---
+++ slack/src/main/java/prj/blockchain/slack/SlackApplication.java
@@ -0,0 +1,13 @@
+package prj.blockchain.slack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackApplication.class, args);
+	}
+
+}
